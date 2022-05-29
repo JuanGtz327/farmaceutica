ae2e6e1c5b2f515cf6b54cdd128c0765d9ff76f3
JuanGtz <jucasaguipn09@gmail.com>
2022-05-29 18:09:31 -0500

    gerente completo

--- routes/gerenteRouter.js
+++ routes/gerenteRouter.js
@@ -9,8 +9,9 @@ const { getTrabajadores, getTrabajadorbyNSS, insertTrabajador, updateTrabajador,
 router.get('/getProductos/:idGer', (req, res) => {
     const { idGer } = req.params;
     getProductos(productos => {
-        res.send(productos);
-    })
+        //res.send(productos);
+        res.render('productos',{idGer,productos});
+    });
 });
 
 router.get('/getProducto/:idProd/:idGer', (req, res) => {
@@ -20,38 +21,51 @@ router.get('/getProducto/:idProd/:idGer', (req, res) => {
     });
 });
 
-router.get('/addProducto/:idGer', (req, res) => {
-    const { idGer } = req.params;
-
-});
-
 router.post('/addProducto/:idGer', (req, res) => {
     const { idGer } = req.params;
     insertProducto(req.body, idGer, (data, err) => {
         if (data == -1) {
             res.send({ err })
         } else {
-            res.send({ msg: 'Producto Insertado' });
+            //res.send({ msg: 'Producto Insertado' });
+            res.redirect(`/gerente/getProductos/${idGer}`);
         }
     })
 });
 
 router.get('/updateProducto/:idProd/:idGer', (req, res) => {
     const { idProd, idGer } = req.params;
-
+    getProductos(productos => {
+        getProductobyId(idProd, producto => {
+            const prodEdit = producto[0];
+            res.render('productos',{idGer,prodEdit,productos});
+        });
+    });
 });
 
 router.post('/updateProducto/:idProd/:idGer', (req, res) => {
     const { idProd, idGer } = req.params;
     updateProducto(req.body, idGer, idProd, data => {
-        res.send({ msg: 'Producto actualizado' });
+        //res.send({ msg: 'Producto actualizado' });
+        res.redirect(`/gerente/getProductos/${idGer}`);
     })
+});
+
+router.get('/deleteProducto/:idProd/:idGer', (req, res) => {
+    const { idProd, idGer } = req.params;
+    getProductos(productos => {
+        getProductobyId(idProd, producto => {
+            const prodDel = producto[0];
+            res.render('productos',{idGer,prodDel,productos});
+        });
+    });
 });
 
 router.post('/deleteProducto/:idProd/:idGer', (req, res) => {
     const { idProd, idGer } = req.params;
     deleteProducto(idProd, data => {
-        res.send({ msg: 'Producto eliminado' });
+        //res.send({ msg: 'Producto eliminado' });
+        res.redirect(`/gerente/getProductos/${idGer}`);
     });
 });
 
@@ -59,7 +73,8 @@ router.post('/deleteProducto/:idProd/:idGer', (req, res) => {
 router.get('/getTrabajadores/:idGer', (req, res) => {
     const { idGer } = req.params;
     getTrabajadores(trabajadores => {
-        res.send(trabajadores);
+        //res.send(trabajadores);
+        res.render('trabajadores',{trabajadores,idGer});
     })
 });
 
@@ -70,38 +85,51 @@ router.get('/getTrabajador/:idTrab/:idGer', (req, res) => {
     });
 });
 
-router.get('/addTrabajador/:idGer', (req, res) => {
-    const { idGer } = req.params;
-
-});
-
 router.post('/addTrabajador/:idGer', (req, res) => {
     const { idGer } = req.params;
     insertTrabajador(req.body, idGer, (data, err) => {
         if (data == -1) {
             res.send({ err })
         } else {
-            res.send({ msg: 'trabajador Insertado' });
+            //res.send({ msg: 'trabajador Insertado' });
+            res.redirect(`/gerente/getTrabajadores/${idGer}`);
         }
     })
 });
 
 router.get('/updateTrabajador/:idTrab/:idGer', (req, res) => {
-    const { idProd, idGer } = req.params;
-
+    const { idTrab, idGer } = req.params;
+    getTrabajadores(trabajadores => {
+        getTrabajadorbyNSS(idTrab, trabajador => {
+            const trabEdit = trabajador[0];
+            res.render('trabajadores',{idGer,trabEdit,trabajadores});
+        });
+    });
 });
 
 router.post('/updateTrabajador/:idTrab/:idGer', (req, res) => {
     const { idTrab, idGer } = req.params;
     updateTrabajador(req.body, idGer, idTrab, data => {
-        res.send({ msg: 'trabajador actualizado' });
+        //res.send({ msg: 'trabajador actualizado' });
+        res.redirect(`/gerente/getTrabajadores/${idGer}`);
     })
+});
+
+router.get('/deleteTrabajador/:idTrab/:idGer', (req, res) => {
+    const { idTrab, idGer } = req.params;
+    getTrabajadores(trabajadores => {
+        getTrabajadorbyNSS(idTrab, trabajador => {
+            const trabDel = trabajador[0];
+            res.render('trabajadores',{idGer,trabDel,trabajadores});
+        });
+    });
 });
 
 router.post('/deleteTrabajador/:idTrab/:idGer', (req, res) => {
     const { idTrab, idGer } = req.params;
     deleteTrabajador(idTrab, data => {
-        res.send({ msg: 'trabajador eliminado' });
+        //res.send({ msg: 'trabajador eliminado' });
+        res.redirect(`/gerente/getTrabajadores/${idGer}`);
     });
 });
 
@@ -110,7 +138,9 @@ router.get('/getGanancias/:idGer', (req, res) => {
     const { idGer } = req.params;
     const actDate = new Date(Date.now()).toISOString().substring(0, 10);
     getTotalTickets(actDate, total => {
-        res.send({ msg: `Total de la venta actual ${total[0].Total}` });
+        //res.send({ msg: `Total de la venta actual ${total[0].Total}` });
+        let msg = `Total de la venta actual $${total[0].Total}`;
+        res.render('ganancias',{msg})
     });
 });
 
